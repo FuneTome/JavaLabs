5d7dc0d743e8930cf9dcec3edbbc5b7f6e40e922
Fune_Tome <kurilenkof1905@yandex.ru>
2026-02-23 14:03:06 +0300

    First commit

---
+++ lab2/src/RecIntegral.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class RecIntegral {
+    private Main.Table lastRecords;
+
+    public RecIntegral(Main.Table o) {
+        lastRecords = o;
+    }
+
+    public Main.Table getLastRecords() {
+        return lastRecords;
+    }
+}
